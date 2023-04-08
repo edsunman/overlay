fed43bf0d888e8820af6fbed32d4fac91ecfb1f4
Ed Sunman <ed.sunman@freshlabel.co.uk>
2023-04-08 21:16:10 +0100

    changed logging

--- src/lib/client/pusherClient.ts
+++ src/lib/client/pusherClient.ts
@@ -2,9 +2,9 @@ import Pusher from 'pusher-js';
 
 export function bind(eventId : string , callBack : (data : string) => void) {
 
-    if (process.env.NODE_ENV === "developent") {
+   // if (process.env.NODE_ENV === "developent") {
         Pusher.logToConsole = true;
-    }
+   // }
 
     const pusher = new Pusher('9f8fa613096874482055', { cluster: 'eu' });
     const channel = pusher.subscribe('my-channel');
