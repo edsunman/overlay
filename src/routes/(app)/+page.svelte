05ef6b6088c806d57a3e20c4474cce2f5cb969d2
Ed Sunman <ed.sunman@freshlabel.co.uk>
2023-04-17 19:52:28 +0100

    logout link

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -34,11 +34,15 @@
 </script>
 
 <div class="container mx-auto max-w-md md:pt-24 pt-10 p-5">
-    <h2 class="md:text-8xl text-4xl md:pb-20 pb-10 text-center text-neutral-300">overlay</h2>
+    <h2 class="md:text-8xl text-4xl md:pb-20 pb-10 text-center text-neutral-300">overlay<small
+     class="transition-all duration-100 ease-linear hover:text-neutral-200 text-xl text-neutral-500 sm:inline hidden">
+            <a href="/logout" data-sveltekit-reload >/logout</a>
+        </small>
+    </h2>
 
     {#each events as event}
-        <div class="md:pb-5 pb-2 overflow-hidden whitespace-nowrap ">
-            <button class="md:text-4xl text-2xl">
+        <div class="md:pb-5 pb-2">
+            <button class="md:text-4xl text-2xl text-left">
                 <a data-sveltekit-preload-data="hover" 
                 class="transition-all duration-100 ease-linear text-neutral-300 hover:text-white" href="/{event.id}">{event.name}</a>
             </button>
@@ -48,7 +52,9 @@
     <div class="md:pb-5 pb-2">
     {#if addingNewEvent===false }
         <button on:click={showAddNewEvent} class="
-            transition-all duration-100 ease-linear md:text-4xl text-2xl text-neutral-700 hover:text-neutral-400">+ new</button>  
+            text-left transition-all duration-100 ease-linear md:text-4xl text-2xl text-neutral-700 hover:text-neutral-400">
+            {events.length > 0 ? '+ new' : '+ create an event'}
+        </button>  
     {:else}
         <form on:submit|preventDefault={addNewEvent} >
             <!-- svelte-ignore a11y-autofocus -->   
